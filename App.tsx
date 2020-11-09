f0117ac8fb340ce77132edadc571969fd0eadac4
Lucas Pinheiro Bezerra <lucas.pbezerra@hotmail.com>
2020-11-09 11:26:26 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
